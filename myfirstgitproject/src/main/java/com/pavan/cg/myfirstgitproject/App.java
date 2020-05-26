dcda24139245be2533d1402a33b18dba850f4112
pavansaigopala <pavan-sai.gopala@capgemini.com>
2020-05-26 12:34:08 +0530

    this is my first git commit

---
+++ myfirstgitproject/src/main/java/com/pavan/cg/myfirstgitproject/App.java
@@ -0,0 +1,13 @@
+package com.pavan.cg.myfirstgitproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
